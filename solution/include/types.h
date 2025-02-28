70a1f224612755f24cf37c450adbaa7b11675794
Alan W <awangc31s@gmail.com>
2025-02-28 02:59:03 -0500

    SDLKJF

--- solution/include/types.h
+++ solution/include/types.h
@@ -6,6 +6,7 @@
 #include <vector>
 
 using Assignment = std::unordered_map<int, bool>;
+using WatchedLiterals = std::unordered_map<int, vector<Clause *>>;
 using Clause = std::unordered_set<int>;
 using CNFFormula = std::vector<Clause>;
 
